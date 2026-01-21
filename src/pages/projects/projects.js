4d52ce24b3e35d6481528b12c7fbd976e76daf9b
rylieh31 <121984095+rylieh31@users.noreply.github.com>
2026-01-21 17:30:14 -0500

    removed placeholder

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -3,7 +3,6 @@
 import React from "react";
 import "./projects.css";
 import SafeTrek from "../../images/SafeTrek.JPG";
-import Placeholder from "../../images/projects placeholder.webp"
 import OnAir from "../../images/On Air Schematic.jpg"
 import Walter from "../../images/WalTer.JPG"
 import MyWebsite from "../../images/My Website.png"
